e7c9efd7d4f60f951b789f46983ab0597d589fc8
unknown <mahendraprajapati621@gmail.com>
2021-03-20 12:39:44 +0530

    first commit

---
+++ codeigniter/application/config/calendar.php
@@ -0,0 +1,5 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['client_id'] = 'xxxxxxxxxxxxx.apps.googleusercontent.com';
+$config['client_secret'] = '-xxxxxxxxxxxxx';
